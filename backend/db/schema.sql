efea743bc85d0616577e72fdb0250a7af1a6a7e3
TylerHarnish5 <gamertagtyler@gmail.com>
2026-02-02 14:24:37 -0500

    added tables to the schema

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,6 +1,19 @@
 CREATE TABLE IF NOT EXISTS events (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    timestamp TEXT NOT NULL,
+    e_timestamp TEXT NOT NULL,
     motion_area INTEGER NOT NULL,
     snapshot TEXT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS cameras (
+    c_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    c_name VARCHAR(20),
+    source_type VARCHAR(20),
+    enabled BOOLEAN NOT NULL,
+)
+
+CREATE TABLE IF NOT EXISTS settings(
+    settings_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    settings_value VARCHAR(20),
+    updated_at TIMESTAMP,
+)
